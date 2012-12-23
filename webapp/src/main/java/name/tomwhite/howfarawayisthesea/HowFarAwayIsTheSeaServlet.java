5c71429fde663d29362d59ec72685126028fbb88
Tom White <tom@cloudera.com>
2012-12-23 22:46:27 +0000

    Add JSON. Add index.html page.

--- webapp/src/main/java/name/tomwhite/howfarawayisthesea/HowFarAwayIsTheSeaServlet.java
+++ webapp/src/main/java/name/tomwhite/howfarawayisthesea/HowFarAwayIsTheSeaServlet.java
@@ -1,20 +1,30 @@
 package name.tomwhite.howfarawayisthesea;
 
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HowFarAwayIsTheSeaServlet extends HttpServlet {
 
-  private ServletConfig config;
+  private HowFarAwayIsTheSea howfar;
 
   @Override
-  public void init(ServletConfig config) {
-    this.config = config;
+  public void init(ServletConfig config) throws ServletException {
+    ServletContext context = config.getServletContext();
+    try {
+      URL landShapeFile = context.getResource("/WEB-INF/data/land.bin");
+      URL oceanShapeFile = context.getResource("/WEB-INF/data/ocean.bin");
+      howfar = new HowFarAwayIsTheSea(landShapeFile, oceanShapeFile);
+    } catch (MalformedURLException e) {
+      throw new ServletException(e);
+    }
   }
 
   @Override
@@ -26,18 +36,8 @@ public class HowFarAwayIsTheSeaServlet extends HttpServlet {
     if (lat == null || lat.length() == 0 || lng == null || lng.length() == 0) {
       resp.sendError(404, "Please specify lat/long parameters.");
     } else {
-      ServletContext context = config.getServletContext();
-      URL landShapeFile = context.getResource("/WEB-INF/data/land.bin");
-      URL oceanShapeFile = context.getResource("/WEB-INF/data/ocean.bin");
-
-      HowFarAwayIsTheSea howfar = new HowFarAwayIsTheSea(landShapeFile, oceanShapeFile);
       DistanceToCoast d = howfar.distanceFromCoast(Double.parseDouble(lat), Double.parseDouble(lng));
-      if (d.isOnLand()) {
-        resp.getWriter().println("You are on land!");
-      } else {
-        resp.getWriter().println("You are at sea!");
-      }
-      resp.getWriter().println(d);
+      resp.getWriter().println(new Gson().toJson(d));
     }
   }
 }
