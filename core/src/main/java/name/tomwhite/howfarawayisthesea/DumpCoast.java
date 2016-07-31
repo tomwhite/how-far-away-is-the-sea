b8fd42d1d1beffdb28f0f750f9c7646c3d8b0a3a
Tom White <tom@cloudera.com>
2016-07-31 12:22:50 -0700

    Try Visvalingam-Whyatt algo too.

--- core/src/main/java/name/tomwhite/howfarawayisthesea/DumpCoast.java
+++ core/src/main/java/name/tomwhite/howfarawayisthesea/DumpCoast.java
@@ -10,6 +10,7 @@ import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKBReader;
 import com.vividsolutions.jts.io.WKTReader;
 import com.vividsolutions.jts.simplify.DouglasPeuckerSimplifier;
+import com.vividsolutions.jts.simplify.VWSimplifier;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -62,10 +63,8 @@ public class DumpCoast {
       System.exit(1);
     }
 
-    DouglasPeuckerSimplifier simplifier = new DouglasPeuckerSimplifier(britain);
-    simplifier.setDistanceTolerance(0.1); // vary from 0.1 to 4.0
-    Geometry simplifiedBritain = simplifier.getResultGeometry();
-    britain = simplifiedBritain;
+    //britain = DouglasPeuckerSimplifier.simplify(britain, 1.0); // vary from 0.1 to 4.0
+    britain = VWSimplifier.simplify(britain, 1.0); // vary from 0.1 to 4.0
 
     if (meta) {
       System.out.println("#points\tmean side (s)\t\t1/s\t\t\tlength");
